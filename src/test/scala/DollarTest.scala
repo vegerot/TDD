2377a210bc853aecd818703cf59d90c79a39d790
Max Coplan <mchcopl@yahoo.com>
2021-11-17 16:30:09 -0500

    🎨 scalafmt

--- src/test/scala/DollarTest.scala
+++ src/test/scala/DollarTest.scala
@@ -1,21 +1,21 @@
 import org.scalatest._
-import flatspec._
-import matchers._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
 
 class DollarTest extends AnyFlatSpec with should.Matchers {
   "Dollar" should "multiply with scalars" in {
     val five = new Dollar(5)
     val ten = five * 2
-    ten.amount should be (10)
+    ten.amount.should(be(10))
     val fifteen = five * 3
-    fifteen.amount should be (15)
+    fifteen.amount should be(15)
   }
 
   // @@@
   "Dollar" should "be immutable" in {
     val five = new Dollar(5)
     val ten = five * 2
-    five.amount should be (5)
+    five.amount should be(5)
   }
 }
 
