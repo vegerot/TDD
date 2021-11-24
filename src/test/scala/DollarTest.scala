d1224144f7c4d29c12bcc010c60d4f09a5550b80
Max Coplan <mchcopl@yahoo.com>
2021-11-24 16:35:40 -0500

    ✨ Privacy

--- src/test/scala/DollarTest.scala
+++ src/test/scala/DollarTest.scala
@@ -4,44 +4,37 @@ import org.scalatest.matchers._
 
 class DollarTest extends AnyFlatSpec with should.Matchers {
   "Dollar" should "multiply with scalars" in {
-    val five = new Dollar(5)
-    val ten = five * 2
-    ten.amount.should(be(10))
-    val fifteen = five * 3
-    fifteen.amount should be(15)
+    val five = Dollar(5)
+    Dollar(10) shouldEqual five * 2
+    five * 3 shouldEqual Dollar(15)
   }
 
   it should "testEquality" in {
-    new Dollar(5) shouldEqual new Dollar(5)
-  }
-
-  // @me
-  "Dollar" should "be immutable" in {
-    val five = new Dollar(5)
-    val ten = five * 2
-    five.amount should be(5)
+    Dollar(5) shouldEqual Dollar(5)
+    Dollar(6) shouldNot equal(Dollar(5))
   }
 
   // @me
   "Dollars" should "be equal" in {
-    val firstTwo = new Dollar(2)
-    val secondTwo = new Dollar(2)
+    val firstTwo = Dollar(2)
+    val secondTwo = Dollar(2)
 
     firstTwo shouldEqual secondTwo
 
-    val three = new Dollar(3)
+    val three = Dollar(3)
     firstTwo shouldNot equal(three)
   }
 }
 
-class Dollar(var amount: Int) {
+class Dollar(private val amount: Int) {
   def *(multiplier: Int): Dollar = {
-    new Dollar(this.amount * multiplier)
+    Dollar(this.amount * multiplier)
   }
   override def equals(that: Any): Boolean = that match {
     case that: Dollar => this.amount == that.amount
     // am I getting too far ahead of myself?  There's no tests for this!
-    case that /*: Pounds*/ => ??? // TODO
-    case _                 => false
+    case that: Pounds => ??? // TODO
+    case _            => false
   }
 }
+class Pounds() {}
