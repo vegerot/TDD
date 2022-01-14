d88310d8aef5d41e6b2af03d24d397b2f31200f3
Max Coplan <mchcopl@yahoo.com>
2022-01-13 18:07:18 -0800

    ♻️The Root of all Evil

--- src/test/scala/DollarTest.scala
+++ src/test/scala/DollarTest.scala
@@ -2,42 +2,15 @@ import org.scalatest._
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
 
-class DollarTest extends AnyFlatSpec with should.Matchers {
+class Test extends AnyFlatSpec with should.Matchers {
   "Dollar" should "multiply with scalars" in {
     val five: Money = Money.dollar(5)
     Money.dollar(10) shouldEqual five * 2
     five * 3 shouldEqual Money.dollar(15)
   }
-
   it should "testEquality" in {
     Money.dollar(5) shouldEqual Money.dollar(5)
     Money.dollar(6) shouldNot equal(Money.dollar(5))
-  }
-
-}
-
-class FrancTest extends AnyFlatSpec with should.Matchers {
-  "Franc" should "multiply with scalars" in {
-    val five = Money.franc(5)
-    Money.franc(10) shouldEqual five * 2
-    five * 3 shouldEqual Money.franc(15)
-  }
-
-  it should "testEquality" in {
-    Money.franc(5) shouldEqual Money.franc(5)
-    Money.franc(6) shouldNot equal(Money.franc(5))
-  }
-}
-
-class Test extends AnyFlatSpec with should.Matchers {
-  it should "testEquality" in {
-    Money.dollar(5) shouldEqual Money.dollar(5)
-    Money.dollar(6) shouldNot equal(Money.dollar(5))
-    Money.franc(5) shouldEqual Money.franc(5)
-    Money.franc(6) shouldNot equal(Money.franc(5))
-
-    Money.dollar(2) shouldEqual Money(2, "USD")
-    Money.franc(2) shouldEqual Money(2, "CHF")
 
     // May not be true
     Money.franc(5) shouldNot equal(Money.dollar(5))
@@ -69,10 +42,6 @@ class Money(protected val amount: Int, val currency: String) { // more like Curr
 
 object Money {
   def apply(amt: Int, currency: String) = new Money(amt, currency)
-  def dollar(amount: Int = 1) = new Dollar(amount)
-  def franc(amount: Int = 1) = new Franc(amount)
+  def dollar(amount: Int = 1) = new Money(amount, "USD")
+  def franc(amount: Int = 1) = Money(amount, "CHF")
 }
-
-class Dollar(override val amount: Int) extends Money(amount, "USD") {}
-
-class Franc(override val amount: Int) extends Money(amount, "CHF") {}
